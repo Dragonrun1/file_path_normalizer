d14e55c4abd9a642bc3591932b77127f5514e98d
Michael Cummings <mgcummings@yahoo.com>
2014-11-16 15:56:09 -0800

    Added tests for getFpn().

--- tests/Test/FilePathNormalizerTraitTest.php
+++ tests/Test/FilePathNormalizerTraitTest.php
@@ -11,14 +11,32 @@ namespace FilePathNormalizer\Test;
 
 use FilePathNormalizer\FilePathNormalizer;
 use FilePathNormalizer\FilePathNormalizerTrait;
+use PHPUnit_Framework_Exception;
 use PHPUnit_Framework_MockObject_MockObject;
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 
 /**
  * Class FilePathNormalizerTraitTest
  */
 class FilePathNormalizerTraitTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
     /**
      *
      */
@@ -27,6 +45,22 @@ class FilePathNormalizerTraitTest extends PHPUnit_Framework_TestCase
         $fpnt = $this->getMockForTrait('FilePathNormalizer\FilePathNormalizerTrait');
         $this->assertAttributeEmpty('fpn', $fpnt);
         return $fpnt;
+    }
+    /**
+     * @depends testFpnIsEmpty
+     *
+     * @param PHPUnit_Framework_MockObject_MockObject $fpnt
+     *
+     * @throws PHPUnit_Framework_Exception
+     */
+    public function testGetFpnWhenNotSet(
+        PHPUnit_Framework_MockObject_MockObject $fpnt
+    ) {
+        /**
+         * @type FilePathNormalizerTrait $fpnt
+         */
+        $this->assertInstanceOf('FilePathNormalizer\FilePathNormalizerInterface',
+            $this->invokeMethod($fpnt, 'getFpn'));
     }
     /**
      * @depends testFpnIsEmpty
@@ -45,6 +79,7 @@ class FilePathNormalizerTraitTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeInstanceOf('FilePathNormalizer\FilePathNormalizerInterface',
             'fpn', $fpnt);
         $this->assertAttributeSame($fpn, 'fpn', $fpnt);
+        $this->assertSame($fpn, $this->invokeMethod($fpnt, 'getFpn'));
     }
     /**
      * Sets up the fixture, for example, opens a network connection.
