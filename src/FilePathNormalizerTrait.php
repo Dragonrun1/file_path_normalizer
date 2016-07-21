4563ebed21020c7eed0d161053a5dae46134dcd8
Michael Cummings <mgcummings@yahoo.com>
2016-07-21 07:23:41 -0700

    Push updated FPN trait that decided not to join other commit.

--- src/FilePathNormalizerTrait.php
+++ src/FilePathNormalizerTrait.php
@@ -1,8 +1,9 @@
 <?php
+declare(strict_types = 1);
 /**
  * Contains FilePathNormalizerTrait Trait.
  *
- * PHP version 5.4
+ * PHP version 7.0
  *
  * LICENSE:
  * This file is part of file_path_normalizer which is used to normalize PHP file
@@ -25,7 +26,7 @@
  * You should be able to find a copy of this license in the LICENSE file.
  *
  * @copyright 2014-2016 Michael Cummings
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU GPLv2
+ * @license   GPL-2.0
  * @author    Michael Cummings <mgcummings@yahoo.com>
  */
 /**
@@ -41,27 +42,15 @@ namespace FilePathNormalizer;
 trait FilePathNormalizerTrait
 {
     /**
-     * Set the instance of FilePathNormalizerInterface.
-     *
-     * @param FilePathNormalizerInterface|null $value Instance to use.
-     *
-     * @return $this Fluent interface.
-     * @since 0.2.0-dev Added to making using class easier.
-     * @api
-     */
-    public function setFpn(FilePathNormalizerInterface $value = null)
-    {
-        $this->fpn = $value;
-        return $this;
-    }
-    /**
      * Get the instance of FilePathNormalizerInterface.
+     *
+     * Act like one time factory if property has not already been set.
      *
      * @return FilePathNormalizerInterface Return the instance.
      * @since 0.2.0-dev Added to making using class easier.
      * @api
      */
-    protected function getFpn()
+    public function getFpn(): FilePathNormalizerInterface
     {
         if (null === $this->fpn) {
             $this->fpn = new FilePathNormalizer();
@@ -69,9 +58,21 @@ trait FilePathNormalizerTrait
         return $this->fpn;
     }
     /**
-     * Holds the instance of FilePathNormalizerInterface.
+     * Set the instance of FilePathNormalizerInterface.
      *
-     * @type FilePathNormalizerInterface $fpn
+     * @param FilePathNormalizerInterface $value Instance to use.
+     *
+     * @return self
+     * @since 0.2.0-dev Added to making using class easier.
+     * @api
      */
-    protected $fpn;
+    public function setFpn(FilePathNormalizerInterface $value): self
+    {
+        $this->fpn = $value;
+        return $this;
+    }
+    /**
+     * @type FilePathNormalizerInterface $fpn Holds the instance of FilePathNormalizerInterface.
+     */
+    private $fpn;
 }
