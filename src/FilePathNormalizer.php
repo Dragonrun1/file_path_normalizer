f51222ce93b978a8bf4ca2f92f60e7e072460071
Michael Cummings <mgcummings@yahoo.com>
2016-07-25 08:11:42 -0700

    Do some code re-factoring/re-naming for FilePathNormalizer.

--- src/FilePathNormalizer.php
+++ src/FilePathNormalizer.php
@@ -101,38 +101,94 @@ class FilePathNormalizer implements FilePathNormalizerInterface, PathInfoAwareIn
      *                                                    path with a file name.
      * @return string Returns the normalized path.
      * @throws \DomainException
-     * @throws \LogicException
      * @since 0.2.0-dev Added to making using class easier.
      * @api
      */
     public function normalizePath(string $path, int $options = self::MODE_DEFAULT): string
     {
-        $this->optionsChecks($options);
+        $this->validateOptions($options);
         $pathInfo = $this->getPathInfo();
         $pathInfo->initAll($path);
-        $this->wrapperChecks($pathInfo->getWrappers(), $options);
-        $this->absoluteChecks($pathInfo->getRoot(), $options);
-        $path = $pathInfo->getWrappers() . $pathInfo->getRoot()
-            . implode('/', $this->cleanPartsPath());
-        if (false === strrpos($path, '/', -1)) {
-            $path .= '/';
+        $this->checkWrappers($options);
+        $this->absoluteChecks($options);
+        $path = '';
+        if ($pathInfo->hasWrappers()) {
+            $path .= implode('://', $pathInfo->getWrapperList()) . '://';
         }
+        $path .= $pathInfo->getRoot() . implode('/', $this->cleanPartsPath()) . '/';
         return $path;
     }
     /**
-     * @param string $root
      * @param int    $options
      *
      * @throws \DomainException
      */
-    protected function absoluteChecks(string $root, int $options)
+    protected function absoluteChecks(int $options)
     {
-        if (($options & self::ABSOLUTE_DISABLED) && '' !== $root) {
+        if (($options & self::ABSOLUTE_DISABLED) && $this->getPathInfo()
+                                                         ->isAbsolutePath()
+        ) {
             $mess = 'Given absolute path when absolute was disabled';
             throw new \DomainException($mess);
         }
-        if (($options & self::ABSOLUTE_REQUIRED) && '' === $root) {
+        if (($options & self::ABSOLUTE_REQUIRED) && !$this->getPathInfo()
+                                                          ->isAbsolutePath()
+        ) {
             $mess = 'Absolute path required but root part missing';
+            throw new \DomainException($mess);
+        }
+    }
+    /**
+     * Use to make check on the wrapper part of path.
+     *
+     * @param int $options Options use in checks.
+     *
+     * @throws \DomainException
+     * @since 1.1.0-dev Absolute required to options conversion.
+     */
+    protected function checkWrappers(int $options)
+    {
+        $hasWrappers = $this->getPathInfo()
+                            ->hasWrappers();
+        if (($options & self::WRAPPER_DISABLED) && $hasWrappers) {
+            $mess = 'Given wrapper when wrapper(s) are disabled';
+            throw new \DomainException($mess);
+        }
+        if (($options & self::WRAPPER_REQUIRED) && !$hasWrappers) {
+            $mess = 'Missing wrapper when wrapper(s) are required';
+            throw new \DomainException($mess);
+        }
+        $wrappers = $this->getPathInfo()
+                         ->getWrapperList();
+        $hasVfs = in_array('vfs', $wrappers, true);
+        if (($options & self::VFS_DISABLED) && $hasVfs) {
+            $mess = 'Found vfsStream wrapper when it was disabled';
+            throw new \DomainException($mess);
+        }
+        if (($options & self::VFS_REQUIRED) && !$hasVfs) {
+            $mess = 'Missing vfsStream wrapper when it was required';
+            throw new \DomainException($mess);
+        }
+        /** @noinspection LowPerformanceArrayUniqueUsageInspection */
+        /*
+         * Though technically allowed duplicate wrappers are most likely to be
+         * user errors, programming bugs, or at least a bad programming smell.
+         */
+        if (count($wrappers) !== count(array_unique($wrappers))) {
+            $mess = 'Duplicate wrappers are not allowed';
+            throw new \DomainException($mess);
+        }
+        $regExp = '%^[[:alpha:]][[:alnum:]]+$%';
+        if (false === array_reduce($wrappers, function ($carry, $item) use ($regExp) {
+                return $carry && preg_match($regExp, $item);
+            }, true)
+        ) {
+            $mess = 'Invalidly formatted wrapper name found';
+            throw new \DomainException($mess);
+        }
+        $last = array_pop($wrappers);
+        if ($hasVfs && 'vfs' !== $last) {
+            $mess = 'Must use vfsStream as last wrapper';
             throw new \DomainException($mess);
         }
     }
@@ -146,8 +202,9 @@ class FilePathNormalizer implements FilePathNormalizerInterface, PathInfoAwareIn
     protected function cleanPartsPath(): array
     {
         $parts = [];
-        foreach ($this->pathInfo->getDirList() as $part) {
-            if ('.' === $part || '' === $part) {
+        foreach ($this->getPathInfo()
+                      ->getDirList() as $part) {
+            if ('.' === $part) {
                 continue;
             }
             if ('..' === $part) {
@@ -160,7 +217,7 @@ class FilePathNormalizer implements FilePathNormalizerInterface, PathInfoAwareIn
                  */
                 if (count($parts) < 1) {
                     $mess = 'Unusual above root path found';
-                    throw new \DomainException($mess, 1);
+                    throw new \DomainException($mess);
                 }
                 array_pop($parts);
                 continue;
@@ -174,7 +231,7 @@ class FilePathNormalizer implements FilePathNormalizerInterface, PathInfoAwareIn
      *
      * @throws \DomainException
      */
-    protected function optionsChecks(int $options)
+    protected function validateOptions(int $options)
     {
         // Forbidden combos.
         $combos = [
@@ -199,59 +256,5 @@ class FilePathNormalizer implements FilePathNormalizerInterface, PathInfoAwareIn
             }
         }
         // Pointless combos.
-    }
-    /**
-     * Use to make check on the wrapper part of path.
-     *
-     * @param string $wrapper Wrapper to be checked.
-     * @param int    $options Options use in checks.
-     *
-     * @throws \DomainException
-     * @since 1.1.0-dev Absolute required to options conversion.
-     */
-    protected function wrapperChecks(string $wrapper, int &$options)
-    {
-        if (($options & self::WRAPPER_DISABLED) && '' !== $wrapper) {
-            $mess = 'Given wrapper when wrapper(s) are disabled';
-            throw new \DomainException($mess);
-        }
-        if (($options & self::WRAPPER_REQUIRED) && '' === $wrapper) {
-            $mess = 'Missing wrapper when wrapper(s) are required';
-            throw new \DomainException($mess);
-        }
-        $hasVfs = false !== strpos($wrapper, 'vfs://');
-        if (($options & self::VFS_DISABLED) && $hasVfs) {
-            $mess = 'Found vfsStream wrapper when it was disabled';
-            throw new \DomainException($mess);
-        }
-        if (($options & self::VFS_REQUIRED) && !$hasVfs) {
-            $mess = 'Missing vfsStream wrapper when it was required';
-            throw new \DomainException($mess);
-        }
-        $wrappers = explode('://', $wrapper);
-        // Discard empty artifact.
-        array_pop($wrappers);
-        /** @noinspection LowPerformanceArrayUniqueUsageInspection */
-        /*
-         * Though technically allowed duplicate wrappers are most likely to be
-         * user errors, programming bugs, or at least a bad programming smell.
-         */
-        if (count($wrappers) !== count(array_unique($wrappers))) {
-            $mess = 'Duplicate wrappers are not allowed';
-            throw new \DomainException($mess);
-        }
-        $regExp = '%^[[:alpha:]][[:alnum:]]+$%';
-        if (false === array_reduce($wrappers, function ($carry, $item) use ($regExp) {
-                return $carry && preg_match($regExp, $item);
-            }, true)
-        ) {
-            $mess = 'Invalidly formatted wrapper name found';
-            throw new \DomainException($mess);
-        }
-        $last = array_pop($wrappers);
-        if ($hasVfs && 'vfs' !== $last) {
-            $mess = 'Must use vfsStream as last wrapper';
-            throw new \DomainException($mess);
-        }
     }
 }
